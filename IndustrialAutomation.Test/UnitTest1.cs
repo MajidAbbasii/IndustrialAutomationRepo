d83893640b0a86542e35451a0e52cf7c2a2aca0e
pishahang <b.pishahang@gmail.com>
2023-07-07 13:27:45 +1000

    creating solution

---
+++ IndustrialAutomation.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IndustrialAutomation.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
