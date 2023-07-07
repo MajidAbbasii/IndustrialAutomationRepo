cf037e637f401aeac5d744c937706e860ebed4ca
pishahang <b.pishahang@gmail.com>
2023-07-07 17:03:38 +1000

    commit Application classes

---
+++ Application/ObjectState/ObjectStateRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ObjectState
+{
+    public class ObjectStateRepository:IObjectStateRepository
+    {
+        public ObjectStateRepository()
+        {
+            
+        }
+    }
+}
