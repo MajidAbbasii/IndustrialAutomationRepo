b9e1b45a0f0e6dd8d4273606a935545e318b88be
pishahang <b.pishahang@gmail.com>
2023-07-07 16:38:00 +1000

    commit object state service model

---
+++ ServiceModel/ObjectState/ObjectStateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceModel.ObjectState
+{
+    public class ObjectStateResponse
+    {
+        public ObjectStateResponse()
+        {
+            
+        }
+    }
+}
